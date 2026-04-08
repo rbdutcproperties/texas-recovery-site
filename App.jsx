5d49a4ae0e45680de3447f6d33dfbf01e21f52ee
rbdutcproperties <isabella@rbdutchproperties.com>
2026-04-08 15:31:39 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function App() {
+export default function App()
   return (
     <div style={{padding: "20px", fontFamily: "Arial"}}>
       <h1>Texas Recovery Residences</h1>
